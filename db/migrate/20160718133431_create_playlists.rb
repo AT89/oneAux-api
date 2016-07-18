b6c5102c42476c0be0c5dda98e06f5b7d84f0a55
Anh Truong <anh.truong.07@cnu.edu>
2016-07-18 10:28:38 -0400

    initial commit, create working

---
+++ db/migrate/20160718133431_create_playlists.rb
@@ -0,0 +1,12 @@
+class CreatePlaylists < ActiveRecord::Migration[5.0]
+  def change
+    create_table :playlists do |t|
+      t.string :name
+      t.string :access_code
+      t.integer :songs_per_user
+      t.references :songs, index: true, array: true
+      t.string :location
+      t.timestamps
+    end
+  end
+end
