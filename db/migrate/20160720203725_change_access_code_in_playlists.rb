5fd30f4d2725ea7a42930465c0a8b70bcc9b7c39
James <james@team-wombat.com>
2016-07-20 16:45:22 -0400

    updated null column change

--- db/migrate/20160720203725_change_access_code_in_playlists.rb
+++ db/migrate/20160720203725_change_access_code_in_playlists.rb
@@ -1,5 +1,5 @@
 class ChangeAccessCodeInPlaylists < ActiveRecord::Migration[5.0]
   def change
-      change_column :playlists, :access_code, null: false
+      change_column :playlists, :access_code, :null => false
   end
 end
