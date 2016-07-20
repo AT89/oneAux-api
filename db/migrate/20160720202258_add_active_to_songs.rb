08f70926810550494bbde0abdac14b3f9bba47a0
James <james@team-wombat.com>
2016-07-20 16:43:19 -0400

    removed extra colon

--- db/migrate/20160720202258_add_active_to_songs.rb
+++ db/migrate/20160720202258_add_active_to_songs.rb
@@ -1,5 +1,5 @@
 class AddActiveToSongs < ActiveRecord::Migration[5.0]
   def change
-    add_column :songs, :active, :boolean, :default: true
+    add_column :songs, :active, :boolean, default: true
   end
 end
